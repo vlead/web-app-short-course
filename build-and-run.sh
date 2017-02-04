2ded5c21771f6fe7b337e2b9bdce7b7b33d19a4f
bollu <siddu.druid@gmail.com>
2017-02-04 16:04:05 +0530

    added quick script that builds and runs from the root folder

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,1 +1,2 @@
-(make build && cd build/code && python runtime/rest/app.py && cd ../../) || cd ../../
+# run source venv/bin/activate before running the script
+(make build && export PYTHONPATH=`pwd` && cd build/code && python runtime/rest/app.py && cd ../../) || cd ../../
